4c95a45819499fdfaa2cbfbf423832e611159365
Rob Baxter <Rob Baxter>
2016-02-12 17:50:24 -0500

    updated tests based on latest from piazza thread @466

--- mc2_p1/mc2_p1.unittest1.py
+++ mc2_p1/mc2_p1.unittest1.py
@@ -15,11 +15,19 @@ class Mc2P1Test(unittest.TestCase):
         self.assertEqual(11, final_portfolio_dataframe_length, "Final portfolio dataframe length {} is incorrect".format(final_portfolio_dataframe_length))
         
         cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals)
-        self.assertAlmostEqual(-0.446948390642, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect".format(sharpe_ratio), delta=None)
-        self.assertAlmostEqual(-0.001965, cum_ret[0], 4, "Cumulative return {} is incorrect".format(cum_ret), delta=None)
-        self.assertAlmostEqual(0.00634128215394, std_daily_ret[0], 4, "Standard deviation {} is incorrect".format(std_daily_ret), delta=None)
-        self.assertAlmostEqual(-0.000178539446839, avg_daily_ret[0], 4, "Avg dailt return {} is incorrect".format(avg_daily_ret), delta=None)
         
+        expected_value = -0.446948390642
+        self.assertAlmostEqual(expected_value, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect. Expected {}".format(sharpe_ratio[0], expected_value), delta=None)
+        
+        expected_value = -0.001965
+        self.assertAlmostEqual(expected_value, cum_ret[0], 4, "Cumulative return {} is incorrect. Expected {}".format(cum_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.00634128215394
+        self.assertAlmostEqual(expected_value, std_daily_ret[0], 4, "Standard deviation {} is incorrect. Expected {}".format(std_daily_ret[0], expected_value), delta=None)
+        
+        expected_value = -0.000178539446839
+        self.assertAlmostEqual(expected_value, avg_daily_ret[0], 4, "Avg daily return {} is incorrect. Expected {}".format(avg_daily_ret[0], expected_value), delta=None)
+    
         '''
         Sharpe Ratio of Fund: -0.446948390642
         Sharpe Ratio of $SPX: 0.882168679776
@@ -45,11 +53,19 @@ class Mc2P1Test(unittest.TestCase):
         self.assertEqual(240, final_portfolio_dataframe_length, "Final portfolio dataframe length {} is incorrect".format(final_portfolio_dataframe_length))
         
         cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals)
-        self.assertAlmostEqual(1.21540888742, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect".format(sharpe_ratio), delta=None)
-        self.assertAlmostEqual(0.13386, cum_ret[0], 4, "Cumulative return {} is incorrect".format(cum_ret), delta=None)
-        self.assertAlmostEqual(0.00720514136323, std_daily_ret[0], 4, "Standard deviation {} is incorrect".format(std_daily_ret), delta=None)
-        self.assertAlmostEqual(0.000551651296638, avg_daily_ret[0], 4, "Avg dailt return {} is incorrect".format(avg_daily_ret), delta=None)
         
+        expected_value = 1.21540888742
+        self.assertAlmostEqual(expected_value, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect. Expected {}".format(sharpe_ratio[0], expected_value), delta=None)
+        
+        expected_value = 0.13386
+        self.assertAlmostEqual(expected_value, cum_ret[0], 4, "Cumulative return {} is incorrect. Expected {}".format(cum_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.00720514136323
+        self.assertAlmostEqual(expected_value, std_daily_ret[0], 4, "Standard deviation {} is incorrect. Expected {}".format(std_daily_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.000551651296638
+        self.assertAlmostEqual(expected_value, avg_daily_ret[0], 4, "Avg daily return {} is incorrect. Expected {}".format(avg_daily_ret[0], expected_value), delta=None)
+    
         '''
         Sharpe Ratio of Fund: 1.21540888742
         Sharpe Ratio of $SPX: 0.0183389807443
@@ -75,11 +91,19 @@ class Mc2P1Test(unittest.TestCase):
         self.assertEqual(232, final_portfolio_dataframe_length, "Final portfolio dataframe length {} is incorrect".format(final_portfolio_dataframe_length))
 
         cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals)
-        self.assertAlmostEqual(0.788982285751, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect".format(sharpe_ratio), delta=None)
-        self.assertAlmostEqual(0.0787526, cum_ret[0], 4, "Cumulative return {} is incorrect".format(cum_ret), delta=None)
-        self.assertAlmostEqual(0.00711102080156, std_daily_ret[0], 4, "Standard deviation {} is incorrect".format(std_daily_ret), delta=None)
-        self.assertAlmostEqual(0.000353426354584, avg_daily_ret[0], 4, "Avg dailt return {} is incorrect".format(avg_daily_ret), delta=None)
         
+        expected_value = 0.788982285751
+        self.assertAlmostEqual(expected_value, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect. Expected {}".format(sharpe_ratio[0], expected_value), delta=None)
+        
+        expected_value = 0.0787526
+        self.assertAlmostEqual(expected_value, cum_ret[0], 4, "Cumulative return {} is incorrect. Expected {}".format(cum_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.00711102080156
+        self.assertAlmostEqual(expected_value, std_daily_ret[0], 4, "Standard deviation {} is incorrect. Expected {}".format(std_daily_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.000353426354584
+        self.assertAlmostEqual(expected_value, avg_daily_ret[0], 4, "Avg daily return {} is incorrect. Expected {}".format(avg_daily_ret[0], expected_value), delta=None)
+                
         '''
         Sharpe Ratio of Fund: 0.788982285751
         Sharpe Ratio of $SPX: -0.177203019906
@@ -105,24 +129,32 @@ class Mc2P1Test(unittest.TestCase):
         self.assertEqual(141, final_portfolio_dataframe_length, "Final portfolio dataframe length {} is incorrect".format(final_portfolio_dataframe_length))
 
         cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals)
-        expected_value = 0.792286003513
-        self.assertAlmostEqual(expected_value, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect. Expected {}".format(sharpe_ratio, expected_value), delta=None)
-        self.assertAlmostEqual(0.05016, cum_ret[0], 4, "Cumulative return {} is incorrect".format(cum_ret), delta=None)
-        self.assertAlmostEqual(0.00428731360634, std_daily_ret[0], 4, "Standard deviation {} is incorrect".format(std_daily_ret), delta=None)
-        self.assertAlmostEqual(0.00021397693658, avg_daily_ret[0], 4, "Avg dailt return {} is incorrect".format(avg_daily_ret), delta=None)
+        
+        # TODO: Fails
+        expected_value = 1.03455887842
+        self.assertAlmostEqual(expected_value, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect. Expected {}".format(sharpe_ratio[0], expected_value), delta=None)
+        
+        expected_value = 0.05016
+        self.assertAlmostEqual(expected_value, cum_ret[0], 4, "Cumulative return {} is incorrect. Expected {}".format(cum_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.00560508094997
+        self.assertAlmostEqual(expected_value, std_daily_ret[0], 4, "Standard deviation {} is incorrect. Expected {}".format(std_daily_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.000365289198877
+        self.assertAlmostEqual(expected_value, avg_daily_ret[0], 4, "Avg daily return {} is incorrect. Expected {}".format(avg_daily_ret[0], expected_value), delta=None)
         
         '''
-        Sharpe Ratio of Fund: 0.792286003513
-        Sharpe Ratio of $SPX: 0.0183389807443
+        Sharpe Ratio of Fund: 1.03455887842
+        Sharpe Ratio of $SPX: 0.247809335326
         
         Cumulative Return of Fund: 0.05016
-        Cumulative Return of $SPX: -0.0224059854302
+        Cumulative Return of $SPX: 0.0135380980508
         
-        Standard Deviation of Fund: 0.00428731360634
-        Standard Deviation of $SPX: 0.0149716091522
+        Standard Deviation of Fund: 0.00560508094997
+        Standard Deviation of $SPX: 0.00840618502785
         
-        Average Daily Return of Fund: 0.00021397693658
-        Average Daily Return of $SPX: 1.7295909534e-05
+        Average Daily Return of Fund: 0.000365289198877
+        Average Daily Return of $SPX: 0.000131224926273
         
         Final Portfolio Value: 1050160.0
         '''
@@ -134,13 +166,21 @@ class Mc2P1Test(unittest.TestCase):
         final_portfolio_dataframe_length = len(portvals)
 
         self.assertAlmostEqual(1050160.0, final_portfolio_value, 4, "Final portfolio value is {} incorrect".format(final_portfolio_value), delta=None)
-        self.assertEqual(101, final_portfolio_dataframe_length, "Final portfolio dataframe length {} is incorrect".format(final_portfolio_dataframe_length))
+        self.assertEqual(106, final_portfolio_dataframe_length, "Final portfolio dataframe length {} is incorrect".format(final_portfolio_dataframe_length))
         
         cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals)
-        self.assertAlmostEqual(1.19402406143, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect".format(sharpe_ratio), delta=None)
-        self.assertAlmostEqual(0.05016, cum_ret[0], 4, "Cumulative return {} is incorrect".format(cum_ret), delta=None)
-        self.assertAlmostEqual(0.00647534272091, std_daily_ret[0], 4, "Standard deviation {} is incorrect".format(std_daily_ret), delta=None)
-        self.assertAlmostEqual(0.000487052265169, avg_daily_ret[0], 4, "Avg dailt return {} is incorrect".format(avg_daily_ret), delta=None)
+        
+        expected_value = 1.19402406143
+        self.assertAlmostEqual(expected_value, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect. Expected {}".format(sharpe_ratio[0], expected_value), delta=None)
+        
+        expected_value = 0.05016
+        self.assertAlmostEqual(expected_value, cum_ret[0], 4, "Cumulative return {} is incorrect. Expected {}".format(cum_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.00647534272091
+        self.assertAlmostEqual(expected_value, std_daily_ret[0], 4, "Standard deviation {} is incorrect. Expected {}".format(std_daily_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.000487052265169
+        self.assertAlmostEqual(expected_value, avg_daily_ret[0], 4, "Avg daily return {} is incorrect. Expected {}".format(avg_daily_ret[0], expected_value), delta=None)
         
         '''
         Sharpe Ratio of Fund: 1.19402406143
@@ -158,8 +198,6 @@ class Mc2P1Test(unittest.TestCase):
         Final Portfolio Value: 1050160.0
         '''
         
-        
-        
     def test_orders_leverage_2(self):
         portvals = marketsim.compute_portvals(orders_file = "./orders/orders-leverage-2.csv", start_val=1000000)
         final_portfolio_value = portvals.ix[-1,:][0]
@@ -169,10 +207,18 @@ class Mc2P1Test(unittest.TestCase):
         self.assertEqual(37, final_portfolio_dataframe_length, "Final portfolio dataframe length {} is incorrect".format(final_portfolio_dataframe_length))
         
         cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(portvals)
-        self.assertAlmostEqual(4.92529481246, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect".format(sharpe_ratio), delta=None)
-        self.assertAlmostEqual(0.07465, cum_ret[0], 4, "Cumulative return {} is incorrect".format(cum_ret), delta=None)
-        self.assertAlmostEqual(0.00651837064888, std_daily_ret[0], 4, "Standard deviation {} is incorrect".format(std_daily_ret), delta=None)
-        self.assertAlmostEqual(0.00202241842159, avg_daily_ret[0], 4, "Avg dailt return {} is incorrect".format(avg_daily_ret), delta=None)
+        
+        expected_value = 4.92529481246
+        self.assertAlmostEqual(expected_value, sharpe_ratio[0], 4, "Sharpe ratio {} is incorrect. Expected {}".format(sharpe_ratio[0], expected_value), delta=None)
+        
+        expected_value = 0.07465
+        self.assertAlmostEqual(expected_value, cum_ret[0], 4, "Cumulative return {} is incorrect. Expected {}".format(cum_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.00651837064888
+        self.assertAlmostEqual(expected_value, std_daily_ret[0], 4, "Standard deviation {} is incorrect. Expected {}".format(std_daily_ret[0], expected_value), delta=None)
+        
+        expected_value = 0.00202241842159
+        self.assertAlmostEqual(expected_value, avg_daily_ret[0], 4, "Avg dailt return {} is incorrect. Expected".format(avg_daily_ret[0], expected_value), delta=None)
         
         '''
         Sharpe Ratio of Fund: 4.92529481246
